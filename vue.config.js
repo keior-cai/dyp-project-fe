6e09eceaf73e7118ed67037f5f0ad754e4c566e3
chuangjie <chuangjie@wezhuiyi.com>
2020-02-27 19:29:12 +0800

    add some commit

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,7 @@ module.exports = {
     assetsDir: 'static',
     productionSourceMap: false,
     devServer: {
-<<<<<<< HEAD
       disableHostCheck: true
-=======
-			disableHostCheck: true
->>>>>>> refs/remotes/origin/master
         // proxy: {
         //     '/api':{
         //         target:'http://jsonplaceholder.typicode.com',
